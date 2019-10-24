2fc31f6b565eec7940f6d5f75f81e694ce3a8c8b
RonakDelvadiya <34827211+RonakDelvadiya@users.noreply.github.com>
2019-10-24 12:12:52 +0530

    Update serializers.py
    remove comments

--- mgmtapp/serializers.py
+++ mgmtapp/serializers.py
@@ -3,33 +3,14 @@ from rest_framework import serializers
 from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-
 import datetime
 
 
-class DiscussionListSerializer(serializers.ModelSerializer):
-   
+class DiscussionListSerializer(serializers.ModelSerializer):   
     class Meta:
         model = Discussion
 
-
-# class DiscussionListSearchSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=10,required=False)
-    
-    
-
-   
-    
-
-
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    
+class CommentSerializer(serializers.ModelSerializer):   
     class Meta:
         model = Comment
-        exclude = ('created_date', 'modified_date')       
-
- 
-    
-
+        exclude = ('created_date', 'modified_date')
