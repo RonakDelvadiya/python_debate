309c1c60c4ac9fcf84fb6f47b1f75004d3454e56
RonakDelvadiya <34827211+RonakDelvadiya@users.noreply.github.com>
2019-10-24 12:06:26 +0530

    Update views.py
    update comments

--- api/views.py
+++ api/views.py
@@ -15,73 +15,52 @@ from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework import generics
 
-class Disc_type_list(APIView):
-    
 
-    #1. Get all discussions which type is Either Article or Question
-   
+class Disc_type_list(APIView):
+    # Get all discussions which type is Either Article or Question
     def get(self, request, format=None):
         result_list =[]
-        # list1 = Discussion.objects.all().filter(discussion_type='question')
-        # list2 = Discussion.objects.all().filter(discussion_type='article')
-        # for x in list1:
-        #     result_list.append(x)
-        # for x in list2:
-        #     result_list.append(x)
         result_list = Discussion.objects.filter(discussion_type__in=['question','article'])
         discussions = result_list
         serializer = DiscussionListSerializer(discussions, many=True)
-        print serializer.data
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class Get_search_Disc_title(APIView):
-
-    #3. Get all discussions which title matches searched term example: /api/discussion/list/?title=Sports
     
+class Get_search_Disc_title(APIView):
+    # Get all discussions which title matches searched term example: /api/discussion/list/?title=Sports
     def get(self, request, format=None):
         lis=['title']
         lis2 = list(request.GET)
-        print lis
-        print lis2
         if lis == lis2 :
             title = request.GET['title']  
-            print title
             discussion = Discussion.objects.filter(title__icontains=title)
-            print discussion
             serializer = DiscussionListSerializer(discussion, many=True)    
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'please enter title in URL'}, status=status.HTTP_200_OK)
 
-
-
+        
 class Disc_search_by_title_text_disctype(APIView):
-    #4. Discussion should be searched by title, text and discussion_type    
+    # Discussion should be searched by title, text and discussion_type    
     def get(self, request, format=None):
         lis=['discussion_type','text','title']
         lis2=list(request.GET)
-        print lis
-        print lis2[0]
         if lis == lis2:    
             title = request.GET['title']
             text = request.GET['text']
             discussion_type = request.GET['discussion_type']
-            print title
             discussion = Discussion.objects.filter(Q(title__icontains=title) | Q(discussion_type__icontains=discussion_type) | Q(text__icontains=text))   
-            print discussion
             serializer = DiscussionListSerializer(discussion, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'please enter title,text,discussion_type in URL in this sequence   '}, status=status.HTTP_200_OK)
+            return Response({'error': 'please enter title,text,discussion_type in URL in this sequence.'}, status=status.HTTP_200_OK)
 
-
-
+        
 #@authentication_classes((TokenAuthentication,))
 class Create_comment(APIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-
-    #2.Add comment api notes:only logged in user can add comment one user can not add more than 1 
+    # Add comment api notes:only logged in user can add comment one user can not add more than 1 
     def post(self, request, format=None):
         if request.user.is_authenticated():
             data=request.data
@@ -91,7 +70,6 @@ class Create_comment(APIView):
             if comment == True:
                 return Response({'error': 'You already commented earlier.'}, status=status.HTTP_200_OK)
             else:
-                #res= Discussion.objects.get(id =disid)
                 result=Discussion.objects.filter(creater_id__added_by = request.user).exists()
                 if result:
                     return Response({'error': 'You can not comment on your Discussion'}, status=status.HTTP_200_OK)
@@ -102,43 +80,22 @@ class Create_comment(APIView):
                         return Response(serializer.data, status=status.HTTP_201_CREATED)
                     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({'error': 'You are not authenticated'}, status=status.HTTP_200_OK)
+            return Response({'error': 'You are not authenticated!!'}, status=status.HTTP_200_OK)
 
-
-
-
-
-
-
-
-
-
-
-# #1. Get all discussions which type is Either Article or Question
+        
+# Get all discussions which type is Either Article or Question
 @csrf_exempt
 @api_view(['GET'])
 def discussions_list(request):
     result_list =[]
-    # list1 = Discussion.objects.all().filter(discussion_type='question')
-    # list2 = Discussion.objects.all().filter(discussion_type='article')
-    # for x in list1:
-    #     result_list.append(x)
-    # for x in list2:
-    #     result_list.append(x)
     result_list = Discussion.objects.filter(discussion_type__in=['question','article'])
     discussions = result_list
     serializer = DiscussionListSerializer(discussions, many=True)
-    print serializer.data
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
-
-
-
-
-# #2. Add comment api notes:only logged in user can add comment one user can not add more than 1 
-# #   comment in particular discussion user can not comment on his own created discussion
+# Add comment api notes:only logged in user can add comment one user can not add more than 1 
+# comment in particular discussion user can not comment on his own created discussion
 @csrf_exempt
 @api_view(['POST'])
 @authentication_classes((TokenAuthentication,))
@@ -154,7 +111,7 @@ def comment_create(request):
             res= Discussion.objects.get(id =disid)
             result=Discussion.objects.filter(added_by = request.user)
             if res in result:
-                return Response({'error': 'You can not comment on your Discussion'}, status=status.HTTP_200_OK)
+                return Response({'error': 'Sorry !! you can not comment on your own Discussion.'}, status=status.HTTP_200_OK)
             else:
                 serializer = CommentSerializer(data=request.data)
                 if serializer.is_valid():
@@ -164,9 +121,8 @@ def comment_create(request):
     else:
         return Response({'error': 'You are not authenticated'}, status=status.HTTP_200_OK)
 
-
-
-# #3. Get all discussions which title matches searched term example: /api/discussion/list/?title=Sports
+    
+# Get all discussions which title matches searched term example: /api/discussion/list/?title=Sports
 @csrf_exempt
 @api_view(['GET'])
 def DiscussionListSearch(request):
@@ -174,39 +130,25 @@ def DiscussionListSearch(request):
     lis2 = list(request.GET)
     if lis == lis2 :
         title = request.GET['title']            
-        print title
         discussion = Discussion.objects.filter(title__icontains=title)
-        print discussion
         serializer = DiscussionListSerializer(discussion, many=True)    
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
-        return Response({'error': 'please enter title in URL'}, status=status.HTTP_200_OK)
+        return Response({'error': 'Please enter title in URL.'}, status=status.HTTP_200_OK)
 
-
-
-
-
-
-
-#4. Discussion should be searched by title, text and discussion_type
+    
+# Discussion should be searched by title, text and discussion_type
 @csrf_exempt
 @api_view(['GET'])
 def DiscussionListSearchAll(request):
     lis=['discussion_type','text','title']
     lis2=list(request.GET)
-    print lis
-    print lis2
     if lis == lis2:    
         title = request.GET['title']
         text = request.GET['text']
         discussion_type = request.GET['discussion_type']
-        print title
         discussion = Discussion.objects.filter(Q(title__icontains=title) | Q(discussion_type__icontains=discussion_type) | Q(text__icontains=text))   
-        print discussion
         serializer = DiscussionListSerializer(discussion, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
-        return Response({'error': 'please enter title,text,discussion_type in URL in this sequence   '}, status=status.HTTP_200_OK)
-
-
-
+        return Response({'error': 'Please enter title,text,discussion_type in URL, same as like mention in error.'}, status=status.HTTP_200_OK)
